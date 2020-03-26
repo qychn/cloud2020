9ac898093680e59cb9b7f8d986032da9b23760f8
QY <xiaadadad@qq.com>
2020-03-26 19:51:01 +0800

    2

--- cloudalibaba-sentinel-service8401/src/main/java/com/example/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/example/springcloud/controller/FlowLimitController.java
@@ -1,18 +1,25 @@
 package com.example.springcloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("testA")
     public String testA(){
+        //暂停毫秒
+        //try {TimeUnit.MILLISECONDS.sleep(1000);}catch (InterruptedException e){e.printStackTrace();}
         return "---------A";
     }
 
     @GetMapping("testB")
     public String testB(){
+        log.info(Thread.currentThread().getName()+"\t"+"------testB");
         return "---------B";
     }
 }
