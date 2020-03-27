6bbc3111af9b5e56dafa6a7094f2d96a86fe6b5c
QY <xiaadadad@qq.com>
2020-03-27 16:40:45 +0800

    0327

--- cloudalibaba-sentinel-service8401/src/main/java/com/example/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/example/springcloud/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.example.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -35,5 +38,15 @@ public class FlowLimitController {
         log.info("testD 测试异常比例");
         int age=10/0;
         return "------testE";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "dealtestHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2){
+        return "---------testHotKey";
+    }
+    public String dealtestHotKey(String p1, String p2, BlockException e) {
+        return "--------兜底降级方法deal_testHotKey";
     }
 }
