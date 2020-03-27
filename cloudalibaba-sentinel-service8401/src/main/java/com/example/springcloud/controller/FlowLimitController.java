50cee9948c3174760e0f144d70dd787382b56d83
QY <xiaadadad@qq.com>
2020-03-27 10:51:06 +0800

    0327

--- cloudalibaba-sentinel-service8401/src/main/java/com/example/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/example/springcloud/controller/FlowLimitController.java
@@ -22,4 +22,18 @@ public class FlowLimitController {
         log.info(Thread.currentThread().getName()+"\t"+"------testB");
         return "---------B";
     }
+
+    @GetMapping("/testD")
+    public String testD(){
+        //暂停秒
+        try {TimeUnit.SECONDS.sleep(1);}catch (InterruptedException e){e.printStackTrace();}
+        log.info("testD 测试RT 平均响应时间");
+        return "------testD";
+    }
+    @GetMapping("/testE")
+    public String testE(){
+        log.info("testD 测试异常比例");
+        int age=10/0;
+        return "------testE";
+    }
 }
